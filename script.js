cd60054faf64f6c6a5a763f89aba4d8ab36136c1
yhordi <jordanakamin@gmail.com>
2013-11-09 23:06:38 -0800

    5th draft
    math related changes, added a guesses array

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
 
     var num = Math.floor((Math.random() * 100) + 1);
     console.log(num);
-    var guesses = []; 
-
+    var guesses = [];
+    
     $(".temperature").hide();
 
     $("#restart").hide();
@@ -14,58 +14,65 @@ $(document).ready(function () {
 
     $("#guess").click(function () {
         var userAttempt = $("#userGuess").val();
-    
-    if (userAttempt === ""){
-        $("#noString").show();
-    }
-
-    if (userAttempt > 100) {
-        $(".alert").hide();
-        $("#over100").show();
-        $(".temperature").hide();
         guesses.push(parseInt(userAttempt));
-    }
-
-    if (userAttempt < 0) {
-        $(".alert").hide();
-        $("#under1").show();
-        $(".temperature").hide();
-    }
-
-    if (userAttempt < num && userAttempt > 0) {
-        $("#warmer").hide();
-        $("#colder").show("slow",function(){});
-        $("#guesses").append(" " + userAttempt);
-        $(".alert").hide();
-         guesses.push(parseInt(userAttempt));
-    }
-    
-    if (userAttempt > num && userAttempt < 100) {
-        $("#colder").hide();
-        $("#warmer").show("slow", function(){});
-        $("#guesses").append(" " + userAttempt);
-        $(".alert").hide();
-        guesses.push(parseInt(userAttempt));
-    }
-    
-
-    if (userAttempt == num) {
-        $("#congrats").fadeIn("fast");
-        $("#restart").show("slow", function(){});
-    }
+        distance = Math.abs(guesses[guesses.length-1] - num);
+        if (userAttempt === "") {
+            $("#noString").show();
+        } else if (userAttempt > 100) {
+            $(".alert").hide();
+            $("#over100").show();
+            $(".temperature").hide();
+            if (distance < guesses[guesses.length - 2] - num) {
+                alert("getting warmer");
+            } else if (distance > Math.abs(guesses[guesses.length - 2] - num)) {
+                alert("colder, broseph");
+            }
+        } else if (userAttempt < 0) {
+            $(".alert").hide();
+            $("#under1").show();
+            $(".temperature").hide();
+            if (distance < guesses[guesses.length - 2] - num) {
+                alert("getting warmer");
+            } else if (distance > Math.abs(guesses[guesses.length - 2] - num)) {
+                alert("colder, broseph");
+            }
+        } else if (userAttempt < num && userAttempt > 0) {
+            $("#warmer").hide();
+            $("#colder").show("slow", function () {});
+            $("#guesses").append(" " + userAttempt);
+            $(".alert").hide();
+            if (distance < Math.abs(guesses[guesses.length - 2] - num)) {
+                alert("getting warmer");
+            } else if (distance > Math.abs(guesses[guesses.length - 2] - num)) {
+                alert("colder, broseph");
+            }        
+        } else if (userAttempt > num && userAttempt < 100) {
+            $("#colder").hide();
+            $("#warmer").show("slow", function () {});
+            $("#guesses").append(" " + userAttempt);
+            $(".alert").hide();
+            if (distance < Math.abs(guesses[guesses.length - 2] - num)) {
+                alert("getting warmer");
+            } else if (distance > Math.abs(guesses[guesses.length - 2] - num)) {
+                alert("colder, broseph");
+            }
+        } else if (userAttempt == num) {
+            $("#congrats").fadeIn("fast");
+            $("#restart").show("slow", function () {});
+        }
         console.log(guesses);
+        console.log("distance " + distance)
 
     });
-     
+
     $("#restart").click(function () {
-         num = Math.floor((Math.random() * 100) + 1);
-    console.log(num);
+        num = Math.floor((Math.random() * 100) + 1);
+        console.log(num);
         $("#congrats").hide();
         $("#restart").hide();
         $("#noString").hide();
-});   
+        guesses = [];
+    });
 
-	
+
 });
-
-	
