1f304152494609dd510aab79a0eb15eafd5fe88d
yhordi <jordanakamin@gmail.com>
2013-11-09 20:08:32 -0800

    4th draft
    Put the validation code at the top

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
+
     var num = Math.floor((Math.random() * 100) + 1);
     console.log(num);
-    
+    var guesses = []; 
+
     $(".temperature").hide();
 
     $("#restart").hide();
@@ -13,47 +15,56 @@ $(document).ready(function () {
     $("#guess").click(function () {
         var userAttempt = $("#userGuess").val();
     
+    if (userAttempt === ""){
+        $("#noString").show();
+    }
+
+    if (userAttempt > 100) {
+        $(".alert").hide();
+        $("#over100").show();
+        $(".temperature").hide();
+        guesses.push(parseInt(userAttempt));
+    }
+
+    if (userAttempt < 0) {
+        $(".alert").hide();
+        $("#under1").show();
+        $(".temperature").hide();
+    }
+
     if (userAttempt < num && userAttempt > 0) {
         $("#warmer").hide();
         $("#colder").show("slow",function(){});
         $("#guesses").append(" " + userAttempt);
         $(".alert").hide();
+         guesses.push(parseInt(userAttempt));
     }
-    if (userAttempt === ""){
-        $("#noString").show();
-    }
+    
     if (userAttempt > num && userAttempt < 100) {
         $("#colder").hide();
         $("#warmer").show("slow", function(){});
         $("#guesses").append(" " + userAttempt);
         $(".alert").hide();
+        guesses.push(parseInt(userAttempt));
     }
-    if (userAttempt > 100) {
-        $(".alert").hide();
-    	$("#over100").show();
-        $(".temperature").hide();
-    }
+    
 
-    if (userAttempt < 0) {
-        $(".alert").hide();
-    	$("#under1").show();
-        $(".temperature").hide();
-    }
     if (userAttempt == num) {
         $("#congrats").fadeIn("fast");
         $("#restart").show("slow", function(){});
     }
-    
+        console.log(guesses);
 
     });
-     $("#restart").click(function () {
+     
+    $("#restart").click(function () {
          num = Math.floor((Math.random() * 100) + 1);
     console.log(num);
         $("#congrats").hide();
         $("#restart").hide();
         $("#noString").hide();
 });   
-    
+
 	
 });
 
