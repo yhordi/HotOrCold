c7e7555f115f88801a26f5fc1171fe4e3bac8fa6
yhordi <jordanakamin@gmail.com>
2013-11-10 00:53:31 -0800

    7th draft
    mostly style changes

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ $(document).ready(function () {
             $("#congrats").fadeIn("fast");
             $("#restart").show("slow", function () {});
             $(".lowHigh").hide();
+            $(".temperature").hide();
         }
 
         else if (userAttempt === "") {
